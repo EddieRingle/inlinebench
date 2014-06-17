999f92de42c10defa398c01d590f5a8496bcc02c
Eddie Ringle <eddie@eringle.net>
2014-06-17 01:18:35 -0400

    DRY things out. Also prevent loop eliding.
    Signed-off-by: Eddie Ringle <eddie@eringle.net>

--- inlinebench/src/inlinebench.c
+++ inlinebench/src/inlinebench.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
+#include <stdint.h>
 
 #include <libtime/libtime.h>
 
 #include "maths.h"
+
+#define bmin(x, y) ((y) ^ (((x) ^ (y)) & -((x) < y)))
 
 int add_ints__extern_local(int a, int b)
 {
@@ -20,26 +23,31 @@ extern inline
 int mul_ints__extern_inline_local(int a, int b);
 
 struct time_result {
+    int      addresult, mulresult;
     uint64_t addtime, multime;
 };
+
+#define LOOP_MAX 1000
 
 struct time_result test_extern(void)
 {
     struct time_result result;
     uint64_t start, end;
-    int i, r;
+    int i, r = 0;
 
     start = libtime_cpu();
-    for (i = 0, r = 0; i < 1000; i++) {
-        r += add_ints__extern(i, 1000 - i);
+    for (i = 0; i < LOOP_MAX; i++) {
+        r += add_ints__extern(i, r);
     }
     end = libtime_cpu();
+    result.addresult = r;
     result.addtime = libtime_cpu_to_wall(end - start);
     start = libtime_cpu();
-    for (i = 0, r = 0; i < 1000; i++) {
-        r += mul_ints__extern(i, 3);
+    for (i = 0; i < LOOP_MAX; i++) {
+        r += mul_ints__extern(i, r);
     }
     end = libtime_cpu();
+    result.mulresult = r;
     result.multime = libtime_cpu_to_wall(end - start);
 
     return result;
@@ -49,19 +57,21 @@ struct time_result test_extern_local(void)
 {
     struct time_result result;
     uint64_t start, end;
-    int i, r;
+    int i, r = 0;
 
     start = libtime_cpu();
-    for (i = 0, r = 0; i < 1000; i++) {
-        r += add_ints__extern_local(i, 1000 - i);
+    for (i = 0; i < LOOP_MAX; i++) {
+        r += add_ints__extern_local(i, r);
     }
     end = libtime_cpu();
+    result.addresult = r;
     result.addtime = libtime_cpu_to_wall(end - start);
     start = libtime_cpu();
-    for (i = 0, r = 0; i < 1000; i++) {
-        r += mul_ints__extern_local(i, 3);
+    for (i = 0; i < LOOP_MAX; i++) {
+        r += mul_ints__extern_local(i, r);
     }
     end = libtime_cpu();
+    result.mulresult = r;
     result.multime = libtime_cpu_to_wall(end - start);
 
     return result;
@@ -71,19 +81,21 @@ struct time_result test_static_inline(void)
 {
     struct time_result result;
     uint64_t start, end;
-    int i, r;
+    int i, r = 0;
 
     start = libtime_cpu();
-    for (i = 0, r = 0; i < 1000; i++) {
-        r += add_ints__static_inline(i, 1000 - i);
+    for (i = 0; i < LOOP_MAX; i++) {
+        r += add_ints__static_inline(i, r);
     }
     end = libtime_cpu();
+    result.addresult = r;
     result.addtime = libtime_cpu_to_wall(end - start);
     start = libtime_cpu();
-    for (i = 0, r = 0; i < 1000; i++) {
-        r += mul_ints__static_inline(i, 3);
+    for (i = 0; i < LOOP_MAX; i++) {
+        r += mul_ints__static_inline(i, r);
     }
     end = libtime_cpu();
+    result.mulresult = r;
     result.multime = libtime_cpu_to_wall(end - start);
 
     return result;
@@ -93,19 +105,21 @@ struct time_result test_extern_inline(void)
 {
     struct time_result result;
     uint64_t start, end;
-    int i, r;
+    int i, r = 0;
 
     start = libtime_cpu();
-    for (i = 0, r = 0; i < 1000; i++) {
-        r += add_ints__extern_inline(i, 1000 - i);
+    for (i = 0; i < LOOP_MAX; i++) {
+        r += add_ints__extern_inline(i, r);
     }
     end = libtime_cpu();
+    result.addresult = r;
     result.addtime = libtime_cpu_to_wall(end - start);
     start = libtime_cpu();
-    for (i = 0, r = 0; i < 1000; i++) {
-        r += mul_ints__extern_inline(i, 3);
+    for (i = 0; i < LOOP_MAX; i++) {
+        r += mul_ints__extern_inline(i, r);
     }
     end = libtime_cpu();
+    result.mulresult = r;
     result.multime = libtime_cpu_to_wall(end - start);
 
     return result;
@@ -115,19 +129,21 @@ struct time_result test_extern_inline_local(void)
 {
     struct time_result result;
     uint64_t start, end;
-    int i, r;
+    int i, r = 0;
 
     start = libtime_cpu();
-    for (i = 0, r = 0; i < 1000; i++) {
-        r += add_ints__extern_inline_local(i, 1000 - i);
+    for (i = 0; i < LOOP_MAX; i++) {
+        r += add_ints__extern_inline_local(i, r);
     }
     end = libtime_cpu();
+    result.addresult = r;
     result.addtime = libtime_cpu_to_wall(end - start);
     start = libtime_cpu();
-    for (i = 0, r = 0; i < 1000; i++) {
-        r += mul_ints__extern_inline_local(i, 3);
+    for (i = 0; i < LOOP_MAX; i++) {
+        r += mul_ints__extern_inline_local(i, r);
     }
     end = libtime_cpu();
+    result.mulresult = r;
     result.multime = libtime_cpu_to_wall(end - start);
 
     return result;
@@ -135,58 +151,42 @@ struct time_result test_extern_inline_local(void)
 
 #define TRIAL_CNT 10
 
+#define testfn(_ib_fn, _ib_name) \
+    { \
+        accumulator.addresult = 0; \
+        accumulator.addtime = 0; \
+        accumulator.mulresult = 0; \
+        accumulator.multime = 0; \
+        lowest.addtime = UINT32_MAX; \
+        lowest.multime = UINT32_MAX; \
+        for (i = 0; i < TRIAL_CNT; i++) { \
+            tmp = _ib_fn(); \
+            accumulator.addtime += tmp.addtime; \
+            accumulator.multime += tmp.multime; \
+            accumulator.addresult += tmp.addresult; \
+            accumulator.mulresult += tmp.mulresult; \
+            lowest.addtime = bmin(lowest.addtime, tmp.addtime); \
+            lowest.multime = bmin(lowest.multime, tmp.multime); \
+        } \
+        printf(_ib_name " functions:\n"); \
+        printf("Avg. add time: %4lu\t\t(Result: %4d)\n", accumulator.addtime / TRIAL_CNT, accumulator.addresult); \
+        printf("Min. add time: %4lu\t\t(Result: %4d)\n", lowest.addtime, accumulator.addresult); \
+        printf("Avg. mul time: %4lu\t\t(Result: %4d)\n", accumulator.multime / TRIAL_CNT, accumulator.mulresult); \
+        printf("Min. mul time: %4lu\t\t(Result: %4d)\n", lowest.multime, accumulator.mulresult); \
+    }
+
 int main(int argc, char **argv)
 {
-    struct time_result accumulator;
+    struct time_result accumulator, lowest;
     struct time_result tmp;
     int i;
 
     libtime_init();
-    accumulator.addtime = 0;
-    accumulator.multime = 0;
-    for (i = 0; i < TRIAL_CNT; i++) {
-        tmp = test_extern();
-        accumulator.addtime += tmp.addtime;
-        accumulator.multime += tmp.multime;
-    }
-    printf("extern - Avg. add time:\t\t\t%lu\n", accumulator.addtime / TRIAL_CNT);
-    printf("extern - Avg. mul time:\t\t\t%lu\n", accumulator.multime / TRIAL_CNT);
-    accumulator.addtime = 0;
-    accumulator.multime = 0;
-    for (i = 0; i < TRIAL_CNT; i++) {
-        tmp = test_extern_local();
-        accumulator.addtime += tmp.addtime;
-        accumulator.multime += tmp.multime;
-    }
-    printf("local extern - Avg. add time:\t\t%lu\n", accumulator.addtime / TRIAL_CNT);
-    printf("local extern - Avg. mul time:\t\t%lu\n", accumulator.multime / TRIAL_CNT);
-    accumulator.addtime = 0;
-    accumulator.multime = 0;
-    for (i = 0; i < TRIAL_CNT; i++) {
-        tmp = test_static_inline();
-        accumulator.addtime += tmp.addtime;
-        accumulator.multime += tmp.multime;
-    }
-    printf("static inline - Avg. add time:\t\t%lu\n", accumulator.addtime / TRIAL_CNT);
-    printf("static inline - Avg. mul time:\t\t%lu\n", accumulator.multime / TRIAL_CNT);
-    accumulator.addtime = 0;
-    accumulator.multime = 0;
-    for (i = 0; i < TRIAL_CNT; i++) {
-        tmp = test_extern_inline();
-        accumulator.addtime += tmp.addtime;
-        accumulator.multime += tmp.multime;
-    }
-    printf("extern inline - Avg. add time:\t\t%lu\n", accumulator.addtime / TRIAL_CNT);
-    printf("extern inline - Avg. mul time:\t\t%lu\n", accumulator.multime / TRIAL_CNT);
-    accumulator.addtime = 0;
-    accumulator.multime = 0;
-    for (i = 0; i < TRIAL_CNT; i++) {
-        tmp = test_extern_inline_local();
-        accumulator.addtime += tmp.addtime;
-        accumulator.multime += tmp.multime;
-    }
-    printf("local extern inline - Avg. add time:\t%lu\n", accumulator.addtime / TRIAL_CNT);
-    printf("local extern inline - Avg. mul time:\t%lu\n", accumulator.multime / TRIAL_CNT);
+    testfn(test_extern, "extern");
+    testfn(test_extern_local, "local extern");
+    testfn(test_static_inline, "static inline");
+    testfn(test_extern_inline, "extern inline");
+    testfn(test_extern_inline_local, "local extern inline");
 
     return 0;
 }
